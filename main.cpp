57ec60c675857ea2b81a5f0377ac2006a2d01e7e
AliangZhou <1376908681@qq.com>
2017-06-21 00:37:57 +0800

    test 1
    first github repository

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void main()
+{
+	int id;
+	string name;
+	string address;
+	
+	cin >> id >> name >> address;
+	cout << "id:" << id << endl;
+	cout << "name:" << name << endl;
+	cout << "address:" << address << endl;
+	
+	system("pause");
+}
